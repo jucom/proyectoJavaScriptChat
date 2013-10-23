e696c76923e17230e028bee79617fd8a44039e7c
Mabille <gabi.mabs@gmail.com>
2013-10-23 20:51:05 -0300

    Modified a bit the css and modified the chatScript js but it does not work yet

--- chatScript.js
+++ chatScript.js
@@ -1,6 +1,7 @@
 var bouton = document.getElementById('boutonSend');
 var chatBody = document.getElementById('chatBody');
 var chatSide = document.getElementById('chatSide');
+var cursor = 1;
 
 alert('bouton est :' + bouton);
 alert('chatBody est :' + chatBody);
@@ -15,8 +16,28 @@ function timer() {
 	return time;
 }
 
+function updateChat() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', '/chat.java?chatFrom('+cursor+')');
+    
+    xhr.send(null);
+    
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == xhr.DONE) {
+            var received = xhr.responseText;
+            alert(received);
+            var obj = JSON.parse(received);
+            //chatBody.innerHTML += 'update ' + cursor + ' <br/>';
+            //cursor++;
+            cursor += obj.length;
+            //for (int i=0, c=received.length; i<c; i++) {
+            //   chatBody.innerHTML += received[i] + '<br/>';
+            //}
+        }
+    }
+}
 
-var updateChat = setInterval(function(){alert(timer());},1000);
+var updateChatCycle = setInterval(function(){updateChat();},1500);
 
 //to stop the update call this function
 //function stopUpdate() {
