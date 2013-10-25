daf7d8a4f2b9d50388dd6fe34b96aeaa95e193ba
Mabille <gabi.mabs@gmail.com>
2013-10-25 00:10:45 -0300

    Change the script and the stylechat because we want to update chat when we do a send and we want to remove the text from the text area too. Also we want the lines to be nice therefore i updated the css a little bit

--- chatScript.js
+++ chatScript.js
@@ -9,46 +9,23 @@ function sendParticipant() {
     var xhr = new XMLHttpRequest();
     xhr.open('POST', '/chat.java?sendParticipant');
     xhr.send(nameParticipant);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == xhr.DONE) {
+            //IMPLEMENT SET CURSOR
+        }
+    }
 }
 // ask the name of the user if nameParticipant is empty
 if (nameParticipant == " ") {
     nameParticipant = prompt('Entrez votre prenom ? ');
-    var send = sendParticipant();
+    sendParticipant();
 }
-
-
-//alert('bouton est :' + bouton);
-//alert('chatBody est :' + chatBody);
-//alert('chatSide est :' + chatSide);
-//alert('messageToSend est :' + messageToSend);
-//alert('nom :' + nameParticipant);
 
 function chatLine(date,name,message) {
     this.sendTime = date;
     this.name = name;    
     this.message = message;
 }
-
-function sendLine (date,name,message) {
-    //alert('sending line : ' + date + ' ' + name + ':' + message);
-    var line = new chatLine(date,name,message);
-    var sLine = JSON.stringify(line);
-    var xhr = new XMLHttpRequest();
-    //alert(sLine);
-    xhr.open('POST','/chat.java?sendLine');    
-    xhr.send(sLine);
-    if (xhr.readyState == xhr.DONE) {
-       updateChat();
-    }
-}
-
-function timer() {
-	var d = new Date();
-	var time = d.toLocaleTimeString();
-	return time;
-}
-
-bouton.addEventListener('click',function(){sendLine(timer(),nameParticipant,messageToSend.value)},false);
 
 function updateChat() {
     var xhr = new XMLHttpRequest();
@@ -62,15 +39,38 @@ function updateChat() {
             var lines = JSON.parse(received);
             cursor += lines.length;
             for (var i=0, c=lines.length; i<c; i++) {
-                chatBody.innerHTML += lines[i].sendTime + ' ' + lines[i].name + ' : ' + lines[i].message + '<br/>';
+                chatBody.innerHTML += '<span class="sendTime">' + lines[i].sendTime + '</span> ' 
+                chatBody.innerHTML += '<span class="name">' + lines[i].name + '</span> : '
+                chatBody.innerHTML += '<span class="message">' +  lines[i].message + '</span><br/>';
             }
         }
    }
 }
 
-var updateChatCycle = setInterval(function(){updateChat();},3000);
+function sendLine (date,name,message) {
+    var line = new chatLine(date,name,message);
+    var sLine = JSON.stringify(line);
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST','/chat.java?sendLine');    
+    xhr.send(sLine);
+    updateChat();
+}
+
+function eraseMessageToSend() {
+    messageToSend.value = '';
+}
+
+function timer() {
+	var d = new Date();
+	var time = d.toLocaleTimeString();
+	return time;
+}
+
+bouton.addEventListener('click',function(){sendLine(timer(),nameParticipant,messageToSend.value);eraseMessageToSend();},false);
+
+var updateChatCycle = setInterval(function(){updateChat();},5000);
 
 //to stop the update call this function
 //function stopUpdate() {
-//	clearInterval(updateChat);
+//	clearInterval(updateChatCycle);
 //}
